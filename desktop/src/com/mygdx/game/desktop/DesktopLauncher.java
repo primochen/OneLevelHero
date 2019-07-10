e5096ade6b63332a4091f8d3f77658306469dcdd
primo <primochen@outlook.com>
2019-07-10 22:41:39 +0800

    test title

--- desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
+++ desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
@@ -8,7 +8,7 @@ public class DesktopLauncher {
 	public static void main(String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
 		// 나중에 수정 요망
-		config.title = "One Level Hero";
+		config.title = "One Level Hero test 測試";
 		config.width = 1280;
 		config.height = 720;
 		config.resizable = false;
