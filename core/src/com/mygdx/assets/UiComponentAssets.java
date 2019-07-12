5dfe32856dec0662e1263e2a706625f5ebba4efe
primo <primo@outlook.com>
2019-07-12 17:38:47 +0800

    font size 22

--- core/src/com/mygdx/assets/UiComponentAssets.java
+++ core/src/com/mygdx/assets/UiComponentAssets.java
@@ -29,7 +29,7 @@ public class UiComponentAssets implements FileAssetsInitializable {
 		FreeTypeFontGenerator generator = new FreeTypeFontGenerator(Gdx.files.internal("freetypefonts/NotoSansCJKtc-Medium.otf"));
 		FreeTypeFontParameter parameter = new FreeTypeFontParameter();
 		parameter.characters = StaticAssets.Usedchars;//FreeTypeFontGenerator.DEFAULT_CHARS + "ㅏㅑㅓㅕㅗㅛㅜㅠㅡㅣㄱ가갸거겨고교구규그기ㄴ나냐너녀노뇨누뉴느니ㄷ다댜더뎌도됴두듀드디ㄹ라랴러려로료루류르리ㅁ마먀머며모묘무뮤므미ㅂ바뱌버벼보뵤부뷰브비ㅅ사샤서셔소쇼수슈스시ㅇ아야어여오요우유으이ㅈ자쟈저져조죠주쥬즈지ㅊ차챠처쳐초쵸추츄츠치ㅋ카캬커켜코쿄쿠큐크키ㅌ타탸터텨토툐투튜트티ㅍ파퍄퍼펴포표푸퓨프피ㅎ하햐허혀호효후휴흐히ㄲ까꺄꺼껴꼬꾜꾸뀨끄끼ㄸ따땨떠뗘또뚀뚜뜌뜨띠ㅃ빠뺘뻐뼈뽀뾰뿌쀼쁘삐ㅆ싸쌰써쎠쏘쑈쑤쓔쓰씨ㅉ짜쨔쩌쪄쪼쬬쭈쮸쯔찌ㅐㅒㅔㅖㅘㅙㅚㅝㅞㅟㅢ개걔게계과괘괴궈궤귀긔내냬네녜놔놰뇌눠눼뉘늬대댸데뎨돠돼되둬뒈뒤듸래럐레례롸뢔뢰뤄뤠뤼릐매먜메몌뫄뫠뫼뭐뭬뮈믜배뱨베볘봐봬뵈붜붸뷔븨새섀세셰솨쇄쇠숴쉐쉬싀애얘에예와왜외워웨위의재쟤제졔좌좨죄줘줴쥐즤채챼체쳬촤쵀최춰췌취츼캐컈케켸콰쾌쾨쿼퀘퀴킈태턔테톄톼퇘퇴퉈퉤튀틔패퍠페폐퐈퐤푀풔풰퓌픠해햬헤혜화홰회훠훼휘희깨꺠께꼐꽈꽤꾀꿔꿰뀌끠때떄떼뗴똬뙈뙤뚸뛔뛰띄빼뺴뻬뼤뽜뽸뾔뿨쀄쀠쁴쌔썌쎄쎼쏴쐐쐬쒀쒜쒸씌째쨰쩨쪠쫘쫴쬐쭤쮀쮜쯰각간갇갈감갑강낙난낟날남납낭닥단닫달담답당락란랃랄람랍랑막만맏말맘맙망박반받발밤밥방삭산삳살삼삽상악안앋알암압앙작잔잗잘잠잡장착찬찯찰참찹창칵칸칻칼캄캅캉탁탄탇탈탐탑탕팍판팓팔팜팝팡학한핟할함합항" +"我";
-		parameter.size = 30;//StaticAssets.Usedchars;//
+		parameter.size = 22;//StaticAssets.Usedchars;//
 		//FreeTypeBitmapFontData fontData = generator.generateData(parameter);
 		font = generator.generateFont(parameter);//new BitmapFont(fontData, fontData.getTextureRegion(), false);
 		//font = new BitmapFont(Gdx.files.internal("skin/hangeul2.fnt"));
